08c3efa5b662d72899a8cfe3276dc1f5ae5849cf
changxinrui <641215320@qq.com>
2023-03-27 18:23:49 +0800

    [add] hybrid and addressable

---
+++ Assets/FrameworkAssets/HybridCLR/Editor/HybridCLR/BuildAssetsCommand.cs
@@ -0,0 +1,72 @@
+﻿using HybridCLR.Editor.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace HybridCLR.Editor
+{
+    public static class BuildAssetsCommand
+    {
+        [MenuItem("HybridCLR/Build/BuildAssetsAndCopyToStreamingAssets")]
+        public static void BuildAndCopyABAOTHotUpdateDlls()
+        {
+            BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
+            CompileDllCommand.CompileDll(target);
+            CopyABAOTHotUpdateDlls(target);
+        }
+
+        public static void CopyABAOTHotUpdateDlls(BuildTarget target)
+        {
+            CopyAOTAssembliesToStreamingAssets();
+            CopyHotUpdateAssembliesToStreamingAssets();
+        }
+
+
+   
+        public static void CopyAOTAssembliesToStreamingAssets()
+        {
+            var target = EditorUserBuildSettings.activeBuildTarget;
+            string aotAssembliesSrcDir = SettingsUtil.GetAssembliesPostIl2CppStripDir(target);
+            string aotAssembliesDstDir = Application.streamingAssetsPath;
+
+            foreach (var dll in SettingsUtil.AOTAssemblyNames)
+            {
+                string srcDllPath = $"{aotAssembliesSrcDir}/{dll}.dll";
+                if (!File.Exists(srcDllPath))
+                {
+                    Debug.LogError($"ab中添加AOT补充元数据dll:{srcDllPath} 时发生错误,文件不存在。裁剪后的AOT dll在BuildPlayer时才能生成，因此需要你先构建一次游戏App后再打包。");
+                    continue;
+                }
+                string dllBytesPath = $"{aotAssembliesDstDir}/{dll}.dll.bytes";
+                File.Copy(srcDllPath, dllBytesPath, true);
+                Debug.Log($"[CopyAOTAssembliesToStreamingAssets] copy AOT dll {srcDllPath} -> {dllBytesPath}");
+            }
+        }
+
+        public static void CopyHotUpdateAssembliesToStreamingAssets()
+        {
+            var target = EditorUserBuildSettings.activeBuildTarget;
+
+            string hotfixDllSrcDir = SettingsUtil.GetHotUpdateDllsOutputDirByTarget(target);
+            string hotfixAssembliesDstDir = Application.streamingAssetsPath;
+#if NEW_HYBRIDCLR_API
+            foreach (var dll in SettingsUtil.HotUpdateAssemblyFilesExcludePreserved)
+#else
+            foreach (var dll in SettingsUtil.HotUpdateAssemblyFiles)
+#endif
+            {
+                string dllPath = $"{hotfixDllSrcDir}/{dll}";
+                string dllBytesPath = $"{hotfixAssembliesDstDir}/{dll}.bytes";
+                File.Copy(dllPath, dllBytesPath, true);
+                Debug.Log($"[CopyHotUpdateAssembliesToStreamingAssets] copy hotfix dll {dllPath} -> {dllBytesPath}");
+            }
+        }
+
+       
+    }
+}
---
+++ Assets/HybridCLRData/Generated/AOTGenericReferences.cs
@@ -0,0 +1,18 @@
+public class AOTGenericReferences : UnityEngine.MonoBehaviour
+{
+
+	// {{ AOT assemblies
+	// UnityEngine.CoreModule.dll
+	// }}
+
+	// {{ constraint implement type
+	// }} 
+
+	// {{ AOT generic types
+	// }}
+
+	public void RefMethods()
+	{
+		// object UnityEngine.GameObject.AddComponent<object>()
+	}
+}
