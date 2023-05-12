8d4c6c8d7f6afaee6e5a0cba873953f5a4309ef4
Aida\changxinrui <641215320@qq.com>
2023-05-12 23:04:13 +0800

    [modify] extent extime

--- Assets/SDevelop/HotfixScripts/Framework/Extension/Ex.ExTime.cs
+++ Assets/SDevelop/HotfixScripts/Framework/Extension/Ex.ExTime.cs
@@ -6,6 +6,11 @@ namespace Ex
 {
     public static class ExTime
     {
+        /// <summary>
+        /// 获取00：00：00格式时间，最高位为小时，最低位为秒
+        /// </summary>
+        /// <param name="time">时长（秒）</param>
+        /// <returns></returns>
         public static string GetTime(float time)
         {
             float h = Mathf.FloorToInt(time / 3600f);
@@ -14,6 +19,11 @@ namespace Ex
             return h.ToString("00") + ":" + m.ToString("00") + ":" + s.ToString("00");
         }
 
+        /// <summary>
+        /// 获取00：00：00：00格式时间，最高位为day，最低位为秒
+        /// </summary>
+        /// <param name="time">时长（秒）</param>
+        /// <returns></returns>
         public static string GetTimeToDay(float time)
         {
             float d = Mathf.FloorToInt(time / 86400f);
@@ -26,7 +36,7 @@ namespace Ex
         /// <summary>  
         /// 获取当前时间戳  
         /// </summary>  
-        /// <param name="bflag">为真时获取10位时间戳,为假时获取13位时间戳.bool bflag = true</param>  
+        /// <param name="bflag">为真时获取10位时间戳(秒),为假时获取13位时间戳（毫秒）。</param>  
         /// <returns></returns>  
         public static long GetTimeStamp(this DateTime dateTime, bool bflag)
         {
@@ -38,6 +48,22 @@ namespace Ex
                 temp = Convert.ToInt64(ts.TotalMilliseconds);
 
             return temp;
+        }
+
+        /// <summary>
+        /// 获取时间差值，00：00：00格式时间，最高位为小时，最低位为秒
+        /// </summary>
+        /// <param name="start">起始时间</param>
+        /// <param name="end">结束时间</param>
+        /// <returns></returns>
+        public static string GetTimeOffset(DateTime start,DateTime end)
+        {
+            long offset = end.GetTimeStamp(true) - start.GetTimeStamp(true);
+            if (offset<=0)
+            {
+                return "00:00:00";
+            }
+            return  GetTime(offset);
         }
     }
 }
