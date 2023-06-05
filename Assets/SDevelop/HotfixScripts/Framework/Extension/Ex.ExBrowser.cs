63c9d1c548cfcc1e8b16b5cd9754c68fc1543b28
changxinrui <641215320@qq.com>
2023-06-05 16:19:11 +0800

    [modify] exbrowser and webglExpInterface

--- Assets/SDevelop/HotfixScripts/Framework/Extension/Ex.ExBrowser.cs
+++ Assets/SDevelop/HotfixScripts/Framework/Extension/Ex.ExBrowser.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using UnityEngine;
 
 
@@ -13,12 +12,45 @@ namespace Ex
 #endif
 
 
-        public static void OpenURL(string url)
+        public enum OpenTypeURLWebGL
+        {
+            NONE,
+
+            /// <summary>
+            /// 浏览器会另开一个新窗口显示链接
+            /// </summary>
+            _blank,
+
+            /// <summary>
+            /// 在同一框架或窗口中打开所链接的文档。此参数为默认值，通常不用指定。
+            /// </summary>
+            _self,
+
+            /// <summary>
+            /// 将链接的文件载入含有该链接框架的父框架集或父窗口中。如果含有该链接的框架不是嵌套的，则在浏览器全屏窗口中载入链接的文件，就象_self参数一样。
+            /// </summary>
+            _parent,
+
+            /// <summary>
+            /// 在当前的整个浏览器窗口中打开所链接的文档，因而会删除所有框架
+            /// </summary>
+            _top,
+
+            /// <summary>
+            /// 在浏览器的搜索区装载文档，注意，这个功能只在Internet Explorer 5 或者更高版本中适用。
+            /// </summary>
+            _search,
+        }
+
+        public static void OpenURL(string url, OpenTypeURLWebGL openType = OpenTypeURLWebGL.NONE)
         {
 #if UNITY_EDITOR
             Application.OpenURL(url);
 #elif UNITY_WEBGL
-            OpenPage(url);
+            if (OpenTypeURLWebGL.NONE == openType)
+                OpenPage(url);
+            else
+                Application.ExternalEval("window.open('" + url + $"','{openType.ToString()}')");
 #else
             Application.OpenURL(url);
 #endif
---
+++ Packages/WebGLExpInterface/Scripts/Interface/chenxuan/CXInterfaceManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UnityEngine;
+
+namespace WebGLExpInterface
+{
+    public static class CXInterfaceManager
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        [DllImport("__Internal")]
+        private static extern string getUrlParams(string name);
+#endif
+        
+        private static string host
+        {
+            get => "http://123.60.156.121:8001/remote_java/experiment/addExperiment/";
+        }
+
+        public static void AddExperiment(this MonoBehaviour mono, string ilabJson, Action failureCallBack,
+            Action<StatusInfo> successCallBack)
+        {
+            string token = String.Empty;
+#if UNITY_WEBGL && !UNITY_EDITOR
+            token = getUrlParams("token");
+#endif
+
+            if (string.IsNullOrEmpty(token))
+            {
+                Debug.LogWarning("token不存在");
+                failureCallBack?.Invoke();
+                return;
+            }
+
+            DTO_AddExperiment addExperiment = new DTO_AddExperiment(token, ilabJson);
+            string jsonData = LitJson.JsonMapper.ToJson(addExperiment);
+            mono.StartCoroutine(ExpInterfaceBase.WebRequest(UnityWebRequestType.POST, host, jsonData, false, false, () => { }, (result) =>
+            {
+                StatusInfo statusInfo = LitJson.JsonMapper.ToObject<StatusInfo>(result);
+                successCallBack?.Invoke(statusInfo);
+            }, null, null));
+        }
+
+
+        #region DTO
+
+        public class StatusInfo
+        {
+            public int code;
+            public string message;
+            public int data;
+        }
+
+        public class DTO_AddExperiment
+        {
+            /// <summary>
+            /// 客户名
+            /// </summary>
+            public string customName;
+
+            /// <summary>
+            /// 用户账号类型
+            /// </summary>
+            public string accountType;
+
+            /// <summary>
+            /// 用户账号
+            /// </summary>
+            public string accountNumber;
+
+            /// <summary>
+            /// 用户名
+            /// </summary>
+            public string userName;
+
+            /// <summary>
+            /// token
+            /// </summary>
+            public string accessToken;
+
+            /// <summary>
+            /// 实验信息json
+            /// </summary>
+            public string contextJson;
+
+            /// <summary>
+            /// 实验平台的URL
+            /// </summary>
+            public string remoteUrl;
+
+            /// <summary>
+            /// 类型（1：普通学生实验保存  2：评审实验保存）
+            /// </summary>
+            public string type;
+
+            public DTO_AddExperiment(string accessToken, string contextJson)
+            {
+                this.accessToken = accessToken;
+                this.contextJson = contextJson;
+            }
+        }
+
+        #endregion
+    }
+}
