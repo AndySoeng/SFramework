c5b934c836ae9e9ff3135d10ac92e0c28e959cf9
changxinrui <641215320@qq.com>
2023-12-20 14:17:18 +0800

    [modify] 调整ExUnityWebRequest Log

--- Packages/SFramework/Extension/Ex.ExUnityWebRequest.cs
+++ Packages/SFramework/Extension/Ex.ExUnityWebRequest.cs
@@ -20,17 +20,24 @@ namespace Ex
             bool getBase64, UnityAction failureCallBack, UnityAction<string> successCallBack, string[] otherHeaderName,
             string[] otherHeaderValue)
         {
-            //Debug.Log(type.ToString() + "\t" + url + "\t" + jsonData);
-            if (sendBase64)
-            {
-                jsonData = Convert.ToBase64String(Encoding.UTF8.GetBytes(jsonData));
-            }
-
-            byte[] body = Encoding.UTF8.GetBytes(jsonData);
-
             using UnityWebRequest unityWeb = new UnityWebRequest(@url, type.ToString());
 
-            unityWeb.uploadHandler = new UploadHandlerRaw(body);
+            if (!string.IsNullOrEmpty(jsonData))
+            {
+                Debug.Log(url + "\n" + jsonData);
+                if (sendBase64)
+                {
+                    jsonData = Convert.ToBase64String(Encoding.UTF8.GetBytes(jsonData));
+                }
+
+                byte[] body = Encoding.UTF8.GetBytes(jsonData);
+                unityWeb.uploadHandler = new UploadHandlerRaw(body);
+            }
+            else
+            {
+                Debug.Log(url);
+            }
+
             unityWeb.SetRequestHeader("Content-Type", "application/json;charset=utf-8");
             //unityWeb.SetRequestHeader("Authorization", "Bearer " + token);
             if (otherHeaderName != null)
@@ -44,9 +51,10 @@ namespace Ex
             unityWeb.downloadHandler = new DownloadHandlerBuffer();
             yield return unityWeb.SendWebRequest();
 
-            if (unityWeb.result == UnityWebRequest.Result.ProtocolError|| unityWeb.result == UnityWebRequest.Result.ConnectionError|| unityWeb.result == UnityWebRequest.Result.DataProcessingError)
+            if (unityWeb.result == UnityWebRequest.Result.ProtocolError || unityWeb.result == UnityWebRequest.Result.ConnectionError ||
+                unityWeb.result == UnityWebRequest.Result.DataProcessingError)
             {
-                Debug.Log("failure:" + unityWeb.error);
+                Debug.Log("UnityWebRequest Failure:\n" + unityWeb.error);
                 failureCallBack?.Invoke();
                 yield break;
             }
@@ -54,13 +62,12 @@ namespace Ex
             if (unityWeb.isDone)
             {
                 string result = unityWeb.downloadHandler.text;
+                Debug.Log("UnityWebRequest Success:\n" + result);
                 if (getBase64)
                 {
                     byte[] c = Convert.FromBase64String(result);
                     result = Encoding.UTF8.GetString(c);
                 }
-
-                //Debug.Log("success:" + result);
                 successCallBack?.Invoke(result);
             }
         }
@@ -69,6 +76,7 @@ namespace Ex
         public static IEnumerator WebRequestFrom(string url, string[] fromKey, string[] fromValue, bool sendBase64,
             bool getBase64, UnityAction failureCallBack, UnityAction<string> successCallBack)
         {
+            Debug.Log(url);
             WWWForm form = new WWWForm();
             if (fromKey != null)
             {
@@ -79,33 +87,33 @@ namespace Ex
                         fromValue[i] = Convert.ToBase64String(Encoding.UTF8.GetBytes(fromValue[i]));
                     }
 
-                    //Debug.Log("表单添加字段：" + fromKey[i] + "\t" + fromValue[i]);
+                    Debug.Log("表单添加字段：" + fromKey[i] + "\t" + fromValue[i]);
                     form.AddField(fromKey[i], fromValue[i]);
                 }
             }
 
-            using  UnityWebRequest unityWeb = UnityWebRequest.Post(@url, form);
+            using UnityWebRequest unityWeb = UnityWebRequest.Post(@url, form);
 
             unityWeb.downloadHandler = new DownloadHandlerBuffer();
             yield return unityWeb.SendWebRequest();
 
-            if (unityWeb.result == UnityWebRequest.Result.ProtocolError|| unityWeb.result == UnityWebRequest.Result.ConnectionError|| unityWeb.result == UnityWebRequest.Result.DataProcessingError)
+            if (unityWeb.result == UnityWebRequest.Result.ProtocolError || unityWeb.result == UnityWebRequest.Result.ConnectionError ||
+                unityWeb.result == UnityWebRequest.Result.DataProcessingError)
             {
-                Debug.Log("failure:" + unityWeb.error);
+                Debug.Log("UnityWebRequest Failure:\n" + unityWeb.error);
                 failureCallBack?.Invoke();
                 yield break;
             }
-            
+
             if (unityWeb.isDone)
             {
                 string result = unityWeb.downloadHandler.text;
+                Debug.Log("UnityWebRequest Success:\n" + result);
                 if (getBase64)
                 {
                     byte[] c = Convert.FromBase64String(result);
                     result = Encoding.UTF8.GetString(c);
                 }
-
-                //Debug.Log("success:" + result);
                 successCallBack?.Invoke(result);
             }
         }
