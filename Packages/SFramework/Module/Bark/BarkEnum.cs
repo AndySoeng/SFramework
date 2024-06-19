9d5e8e47500b0309d0c7b9713ee9e3e15a6387f8
changxinrui <641215320@qq.com>
2024-06-19 11:43:39 +0800

    [add] barksound

--- Packages/SFramework/Module/Bark/BarkEnum.cs
+++ Packages/SFramework/Module/Bark/BarkEnum.cs
@@ -6,14 +6,45 @@
         timeSensitive, //时效性通知，可在专注状态下显示通知。
         passive, //仅将通知添加到通知列表，不会亮屏提醒。
     }
-    
-    
+
+
     public enum BarkSound
     {
-        
+        alarm,
+        anticipate,
+        bell,
+        birdsong,
+        bloom,
+        calypso,
+        chime,
+        ChOO,
+        descent,
+        electronic,
+        fanfare,
+        glass,
+        gotosleep,
+        healthnotification,
+        horn,
+        ladder,
+        mailsent,
+        minuet,
+        multiwayinvitation,
+        newmail,
+        newsflash,
+        rlOlr,
+        paymentsuccess,
+        shake,
+        sherwoodforest,
+        silence,
+        spell,
+        suspense,
+        telegraph,
+        tiptoes,
+        typewriters,
+        update,
     }
-    
-    
+
+
     public enum BarkArchive
     {
         NONE,
