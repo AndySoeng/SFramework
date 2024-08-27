4323e48129ae5ef8ab67c8b451c6f68415c05fb1
changxinrui <641215320@qq.com>
2024-08-27 16:20:11 +0800

    [add] 拓展随机中文名称生成及数字转中文大小写

---
+++ Packages/SFramework/Extension/Ex.ExChinese.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Ex
+{
+    public static class ExChinese
+    {
+        /// <summary>
+        /// 阿拉伯数字转换成中文数字
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static string NumToChinese(string x)
+        {
+            string[] pArrayNum = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+            //string[] pArrayNum = { "零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖" };
+            //为数字位数建立一个位数组
+            string[] pArrayDigit = { "", "十", "百", "千" };
+            //string[] pArrayDigit = { "", "拾", "佰", "扦" };
+            //为数字单位建立一个单位数组
+            string[] pArrayUnits = { "", "万", "亿", "万亿" };
+            var pStrReturnValue = ""; //返回值
+            var finger = 0; //字符位置指针
+            var pIntM = x.Length % 4; //取模
+            int pIntK;
+            if (pIntM > 0)
+                pIntK = x.Length / 4 + 1;
+            else
+                pIntK = x.Length / 4;
+            //外层循环,四位一组,每组最后加上单位: ",万亿,",",亿,",",万,"
+            for (var i = pIntK; i > 0; i--)
+            {
+                var pIntL = 4;
+                if (i == pIntK && pIntM != 0)
+                    pIntL = pIntM;
+                //得到一组四位数
+                var four = x.Substring(finger, pIntL);
+                var P_int_l = four.Length;
+                //内层循环在该组中的每一位数上循环
+                for (int j = 0; j < P_int_l; j++)
+                {
+                    //处理组中的每一位数加上所在的位
+                    int n = Convert.ToInt32(four.Substring(j, 1));
+                    if (n == 0)
+                    {
+                        if (j < P_int_l - 1 && Convert.ToInt32(four.Substring(j + 1, 1)) > 0 && !pStrReturnValue.EndsWith(pArrayNum[n]))
+                            pStrReturnValue += pArrayNum[n];
+                    }
+                    else
+                    {
+                        if (!(n == 1 && (pStrReturnValue.EndsWith(pArrayNum[0]) | pStrReturnValue.Length == 0) && j == P_int_l - 2))
+                            pStrReturnValue += pArrayNum[n];
+                        pStrReturnValue += pArrayDigit[P_int_l - j - 1];
+                    }
+                }
+
+                finger += pIntL;
+                //每组最后加上一个单位:",万,",",亿," 等
+                if (i < pIntK) //如果不是最高位的一组
+                {
+                    if (Convert.ToInt32(four) != 0)
+                        //如果所有4位不全是0则加上单位",万,",",亿,"等
+                        pStrReturnValue += pArrayUnits[i - 1];
+                }
+                else
+                {
+                    //处理最高位的一组,最后必须加上单位
+                    pStrReturnValue += pArrayUnits[i - 1];
+                }
+            }
+
+            return pStrReturnValue;
+        }
+    }
+}
